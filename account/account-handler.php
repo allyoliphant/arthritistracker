2caf323a633918fb9672cc9545c74aea5d5ecde5
Ally Oliphant <aoliphant27@gmail.com>
2019-03-03 19:49:55 -0700

    starting website

---
+++ account/account-handler.php
@@ -0,0 +1,11 @@
+<?php
+
+    // check that the username, password, and email are valid
+    
+    // if invalid
+    // display error message
+
+    // else
+    // update database and display success message
+    header("Location: account.html");  
+    exit();
