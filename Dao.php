06e79b6e6a9af3dd70d0cb7371de0e96ede0754a
Ally Oliphant <aoliphant27@gmail.com>
2019-03-20 22:09:12 -0600

    setting up db and login

---
+++ Dao.php
@@ -0,0 +1,35 @@
+<?php
+    class Dao {
+        private $host = "us-cdbr-iron-east-03.cleardb.net";
+        private $db = "heroku_178e4f084859e1a";
+        private $user = "b4d15cc732a3a0";
+        private $pass = "bcb11a77";
+        
+        private function getConnection () {
+            return new PDO("mysql:host={$this->host};dbname={$this->db}", $this->user, $this->pass);
+        }
+
+        public function getUser($username) {
+            $conn = $this->getConnection();
+            return $conn->query("SELECT * FROM User WHERE Username = '$username'", PDO::FETCH_ASSOC);
+        }
+
+        // return true is username is available, false otherwise
+        public function checkIfAvailable($username) {
+            $conn = $this->getConnection();
+            $result = $conn->query("SELECT Username FROM User WHERE Username = '$username'", PDO::FETCH_ASSOC);
+            return $user->rowCount() <= 0;
+        }
+
+        public function createUser($name, $username, $password, $email) {
+            $conn = $this->getConnection();
+            $saveQuery = "insert into User (Name, Username, Password, Email) values (':name', ':username', ':password', ':email');";
+            $q = $conn->prepare($saveQuery);
+            $q->bindParam(":name", $name);
+            $q->bindParam(":username", $username);
+            $q->bindParam(":password", $password);
+            $q->bindParam(":email", $email);
+            $q->execute();
+        }
+    }
+?>
--- login/login-handler.php
+++ login/login-handler.php
@@ -1,11 +1,24 @@
 <?php
 
-    // verify username and password
-  
-    // if invalid
-    // display error message
+    require_once '../Dao.php';
+    $dao = new Dao();
 
-    // else
-    header("Location: ../home/home.html");  
+    $user = $dao->getUser($_POST['username']); 
+
+    // check that a user was returned
+    if ($user->rowCount() > 0) {
+
+        // get an array of the user indexed by column name
+        $userinfo = $user->fetch(PDO::FETCH_ASSOC);
+
+        // check that the password entered and password returned match
+        if($userinfo['Password'] == $_POST['password']) {
+            header("Location: ../home/home.html");  
+            exit();
+        }
+    }
+
+    header("Location: ./login.html");  
     exit();
+    ?>
 
