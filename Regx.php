bdc73182950f5672e719f312bcdd84b72a969f80
Ally Oliphant <aoliphant27@gmail.com>
2019-04-01 20:33:56 -0600

    trying to fix reguire_once issue - part 2

---
+++ Regx.php
@@ -0,0 +1,35 @@
+<?php
+    class Regx {
+
+        // return true if name fits the pattern, false otherwise
+        public function nameValid ($name) {
+            $pattern = "/^(?=.*[a-zA-Z])[a-zA-Z\s]{1,30}$/";
+            return preg_match($pattern, $name);
+        }
+
+        // return true if username fits the pattern, false otherwise
+        public function usernameValid ($username) {
+            $pattern = "/^[a-zA-Z0-9]{6,30}$/";
+            return preg_match($pattern, $username);
+        }
+
+        // return true if password fits the pattern, false otherwise
+        public function passwordValid ($password) {
+            $pattern = "/(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#\$%&\?\-_])[a-zA-Z0-9!@#\$%&\?\-_]{6,30}/";
+            return preg_match($pattern, $password);
+        }
+
+        // return true if name fits the pattern, false otherwise
+        public function emailValid ($email) {
+            $pattern = "/^[a-zA-Z0-9_\.-]+@[a-zA-Z0-9-\.]+\.[a-zA-Z0-9-]+$/";
+            return preg_match($pattern, $email);
+        }
+
+        // return true if date is a date, false otherwise
+        public function dateValid ($date) {
+            // day, month, or year
+            $pattern = "/ (^[0-9]{4}-[0-9]{2}-[0-9]{2}$) | (^[0-9]{4}-[0-9]{2}$) | (^[0-9]{4}$) /";
+            return preg_match($pattern, $email);
+        }
+    }
+?>
