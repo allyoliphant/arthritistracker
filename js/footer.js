81b230a5d5f5579f009cf684c635bb2d37d15d25
Ally Oliphant <aoliphant27@gmail.com>
2019-04-26 20:14:33 -0600

    size and position of footer

--- js/footer.js
+++ js/footer.js
@@ -5,11 +5,6 @@ $(function() {
     $(".footer").css("top", pageHeight);
     $(".footer-content").css("top", pageHeight);
 
-    $(".footer").css("left", 180);
-    $(".footer-content").css("left", 180);
-    $(".footer").css("right", 20);
-    $(".footer-content").css("right", 20);
-
     $(window).resize(function() {
         pageHeight = $(window).height()*0.94;
 
