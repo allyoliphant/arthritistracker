bcfda8c9fa30f3b95845b3e1cf070dc64730ce65
Ally Oliphant <AlisonOliphant@boisestate.edu>
2019-07-22 12:01:48 -0600

    fix mobile navigation bug

--- js/mobile.js
+++ js/mobile.js
@@ -1,5 +1,12 @@
 $(function() {
 
+  $( window ).resize(function(){ 
+    if ($('.side').css("display") == "block") {
+      $('#menuToggle input').prop( "checked", false );
+      $('.mobile-menu').css("display", "none");
+    }   
+  });
+
   $('#menuToggle').click(function() {
     if ($('.mobile-menu').css("display") == "none") {
       $('.mobile-menu').slideDown("fast");
